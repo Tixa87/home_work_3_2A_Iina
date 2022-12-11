063de6189644a78796691668fa712c25b012adc1
Alexandra Ilina <test@test.ru>
2022-12-11 22:36:32 +0100

    commit2

--- test_hw_3_2.py
+++ test_hw_3_2.py
@@ -1,13 +1,12 @@
-from selene import browser
-from selene.support.conditions import be, have
+from selene.support.shared import browser
+from selene import be, have
 
 
 def test_positive(browser_set):
-
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
     browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in'))
 
-def test_negative(browser_set):
 
+def test_negative(browser_set):
     browser.element('[name="q"]').should(be.blank).type('ghsdjhouhgohjsdkljhssl;gjzhs;zlighuuhn;ln;jn').press_enter()
-    browser.element('[class="s6JM6d"]').should(have.text('ghsdjhouhgohjsdkljhssl;gjzhs;zlighuuhn;ln;jn'))
+    browser.element('[class="s6JM6d"]').should(have.text('test_should_fail'))

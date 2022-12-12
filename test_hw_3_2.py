932d0a30dadd2754922620604e416a4304cc9026
Alexandra Ilina <test@test.ru>
2022-12-12 18:40:21 +0100

    commit3

--- test_hw_3_2.py
+++ test_hw_3_2.py
@@ -3,10 +3,10 @@ from selene import be, have
 
 
 def test_positive(browser_set):
-    browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
+    browser.element('[name="q"]').should(be.blank).type('selene github').press_enter()
     browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in'))
 
 
 def test_negative(browser_set):
     browser.element('[name="q"]').should(be.blank).type('ghsdjhouhgohjsdkljhssl;gjzhs;zlighuuhn;ln;jn').press_enter()
-    browser.element('[class="s6JM6d"]').should(have.text('test_should_fail'))
+    browser.element('[class="card-section"]').should(have.text('No results containing all your search terms were found.'))
